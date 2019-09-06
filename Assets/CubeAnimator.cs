eb517f89d9de791a725925925f394b4931598639
Prof Game <kadomony@gmail.com>
2019-09-06 09:38:30 +0200

    management of animations

---
+++ Assets/CubeAnimator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeAnimator : MonoBehaviour
+{
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (anim.GetBool("to_move"))
+            {
+                anim.SetBool("to_move", false);
+                anim.SetBool("to_rot", true);
+            }
+            else {
+                anim.SetBool("to_move", true);
+                anim.SetBool("to_rot", false);
+            }
+            Debug.Log(anim.GetCurrentAnimatorClipInfo(0)[0].clip.name);
+        }
+    }
+}
